23132c5b89d8581f12acc1c72a1511345b73e944
David Neidhart <david.neidhart@gmx.net>
2022-08-06 21:55:23 +0200

    Better toHex documentation

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,12 @@
-// Add padding to the left of an hex number
-// Example:
-// padLeft(0x2a, 5) => "0002a"
-export const toHex = (n: number, l: number) => n.toString(16).padStart(l, '0');
+/**
+ * Add padding to an hex number
+ * n = 0x2a
+ * l = 5
+ * returned = 0002a
+ * @param n The number you want to add the padding to
+ * @param l The length of the padding (l - n.length)
+ * @returns Hex number with the length l and if needed, padded 0's
+ */
+export const toHex = (n: number, l: number): string => {
+	return n.toString(16).padStart(l, '0');
+}
